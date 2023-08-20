d11b36c3402555fefda8e7077d8ebb63612d9b9d
Nanjila <nanjiladavid2@gmail.com>
2023-08-20 18:46:16 +0300

    The printf task 1

--- _printf.c
+++ _printf.c
@@ -1,1 +1,52 @@
+#include "main.h"
+/**
+  * _printf - function that produce output according to format
+  * @format: the format to be specified
+  * Return:
+  * @...: ellipsis
+  */
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int i;
 
+	va_start(ap, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				int character = va_arg(ap, int);
+
+				putchar(character);
+				i++;
+			}
+			else if (*format == 's')
+			{
+				char *st = va_arg(ap, char*);
+
+				while (*st)
+				{
+					putchar(*st);
+					st++;
+					i++;
+				}
+			}
+			else if (*format == '%')
+			{
+				putchar('%');
+				i++;
+			}
+			else
+			{
+				putchar(*format);
+				i++;
+			}
+			format++;
+		}
+	}
+	va_end(ap);
+	return (i);
+}
